5718cdf6707073b1984628ee1952541387f0cc45
Kim Taeyeong <freeptk1@gmail.com>
2022-03-31 23:45:55 +0900

    feat : UserDao에 기능 추가

--- src/test/java/com/example/tobispring/user/dao/UserDaoTest.java
+++ src/test/java/com/example/tobispring/user/dao/UserDaoTest.java
@@ -1,40 +1,39 @@
 package com.example.tobispring.user.dao;
 
 import com.example.tobispring.user.domain.User;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserDaoTest {
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-            ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-            UserDao dao = context.getBean("userDao", UserDao.class);
+    @Test
+    public void addAndGet() throws SQLException, ClassNotFoundException {
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
 
-            User user = new User();
-            user.setId("dud708");
-            user.setName("김태영");
-            user.setPassword("pa55word");
 
-            dao.add(user);
+        User user = new User();
+        user.setId("dud708");
+        user.setName("김태영");
+        user.setPassword("pa55word");
+        dao.add(user);
 
-            System.out.println(user.getId() + " 등록 성공");
+        System.out.println(user.getId() + " 등록 성공");
 
-            User findUser = dao.get(user.getId());
-            System.out.println(findUser.getName());
-            System.out.println(findUser.getPassword());
+        User findUser = dao.get(user.getId());
+        System.out.println(findUser.getName());
+        System.out.println(findUser.getPassword());
 
-            System.out.println(findUser.getId() + " 조회 성공");
+        System.out.println(findUser.getId() + " 조회 성공");
 
-            if (!user.getName().equals(findUser.getName())) {
-                    System.out.println("테스트 실패 (name)");
-            } else if (!user.getPassword().equals(findUser.getPassword())) {
-                    System.out.println("테스트 실패 (password)");
-            }else{
-                    System.out.println("조회 테스트 성공");
-            }
+        assertThat(user.getName()).isEqualTo(findUser.getName());
+        assertThat(user.getPassword()).isEqualTo(findUser.getPassword());
     }
 }
